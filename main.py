135e3aaed84da0b26eaa66bd9ddbb90178977ff0
stasiudoeringw <13339112-stasiudoeringw@users.noreply.replit.com>
2023-11-13 19:35:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# for i in range(2000,3200):
+#   if i % 7 == 0 and i % 5 != 0:
+#     print(i,end=",")
+
+# liczba = int(input("Podaj liczbę: "))
+# wyniksilnia = 1
+# for j in range(1, liczba+1):
+#     wyniksilnia *= j
+# print(wyniksilnia)
+
+# n=int(input("podaj liczbe"))
+# slowniczek = {}
+# for i in range(1, n + 1):
+#     slowniczek[i]=i*i
+# print(slowniczek)
+
+
+y=input("podaj jakis ciag liczb")
+lista=y.split()
+print(lista)
